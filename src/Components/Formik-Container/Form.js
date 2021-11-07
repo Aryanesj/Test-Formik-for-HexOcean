1a7fd633334ea57ac0d960806e5e94b948254372
Artem <nevadaltd88@gmail.com>
2021-11-07 18:32:37 +0200

    step 2

--- src/Components/Formik-Container/Form.js
+++ src/Components/Formik-Container/Form.js
@@ -1,119 +1,161 @@
 import React from 'react'
-import { Formik, Form } from 'formik';
+import {Formik, Form} from 'formik';
 import * as Yup from 'yup';
 import FormikControl from './FormikControl.js'
 
+
+const ID_PIZZA = 'pizza';
+const ID_SOUP = 'soup';
+const ID_SANDWICH = 'sandwich'
+
 const DishForm = () => {
 
-	const dropdownOptions = [
-		{ key:'Choose a dish', value: '' },
-		{ key:'Pizza', value: 'pizza' },
-		{ key:'Soup', value: 'soup' },
-		{ key:'Sandwich', value: 'sandwich' }
-]
+  const dropdownOptions = [
+    {key: 'Choose a dish', value: ''},
+    {key: 'Pizza', value: ID_PIZZA},
+    {key: 'Soup', value: ID_SOUP},
+    {key: 'Sandwich', value: ID_SANDWICH}
+  ]
 
-	const initialValues = {
-		dish: '',
-		timeDish: '',
-		typeOfDish: '',
-		numberOfSlices: '',
-		diameter: '',
-		sliceBread: '',
-		spicinessScale: ''
-	}
+  const initialValues = {
+    dish: '',
+    timeDish: '',
+    typeOfDish: '',
+    numberOfSlices: '',
+    diameter: '',
+    sliceBread: '',
+    spicinessScale: 6
+  }
 
-	const validationSchema = Yup.object({
-		dish: Yup.string().required('Required'),
-		timeDish: Yup.string().required('Required').nullable(),
-		typeOfDish: Yup.string().required('Required'),
-		numberOfSlices: Yup.number().required('Required'),
-		diameter: Yup.number().required('Required'),
-		sliceBread: Yup.number().required('Required'),
-		spicinessScale: Yup.number().required('Required')
-	})
+  const validationSchema = Yup.object({
+    dish: Yup.string().required('Required'),
+    timeDish: Yup.string().required('Required'),
+    typeOfDish: Yup.string().required('Required'),
+    numberOfSlices: Yup.number().when('typeOfDish', (typeOfDish, schema) => typeOfDish === ID_PIZZA ? schema.required('Required') : schema.nullable()),
+    diameter: Yup.number().when('typeOfDish', (typeOfDish, schema) => typeOfDish === ID_PIZZA ? schema.required('Required') : schema.nullable()),
+    spicinessScale: Yup.number().when('typeOfDish', (typeOfDish, schema) => typeOfDish === ID_SOUP ? schema.required('Required') : schema.nullable()),
+    sliceBread: Yup.number().when('typeOfDish', (typeOfDish, schema) => typeOfDish === ID_SANDWICH ? schema.required('Required') : schema.nullable()),
+  })
 
-	const onSubmit = values => {
-		console.log('Form data', values)
-	}
+  const onSubmit = values => {
+    const commonData = {
+      dish: values.dish,
+      timeDish: values.timeDish,
+      typeOfDish: values.typeOfDish,
+    }
+    let variantData = {};
+    if (values.typeOfDish === ID_PIZZA) {
+      variantData = {
+        numberOfSlices: values.numberOfSlices,
+        diameter: values.diameter
+      }
+    }
 
-	return (
-		<Formik onSubmit={onSubmit} initialValues={initialValues} validationSchema={validationSchema} >
-			{
-				formik => {
-					return <Form className='form-control'>
+    if (values.typeOfDish === ID_SOUP) {
+      variantData = {
+        spicinessScale: values.spicinessScale,
+      }
+    }
 
-					<FormikControl
-						control='input'
-						type='text'
-						label='Dish name'
-						name='dish'
-					/>
+    if (values.typeOfDish === ID_SANDWICH) {
+      variantData = {
+        sliceBread: values.sliceBread,
+      }
+    }
 
-					<FormikControl 
-						control='date'
-						label='Choose a cooking time' 
-						name='timeDish'
-					/>
+    // ФИНАЛЬНЫЙ ОБЪЕКТ С ДАННЫМИ!!!
+    const data = {...commonData, ...variantData};
 
-					<FormikControl
-						className='form-dish'
-						control='select'
-						label='Type of dish'
-						name='typeOfDish'
-						options={dropdownOptions}
-					/>
-{/*---------------------------------------------------------------------------------*/}
-					<FormikControl
-						className='form-nubmer'
-						control ='input'
-						label='Number of slices:'
-						name='numberOfSlices'
-						type='number'
-						placeholder='0'
-						min='0'
-						max="15"
-					/>
+    console.log(data);
+  }
 
+  return (
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      validateOnMount={true}
+    >
+      {
+        formik => {
+          // console.log(formik.values);
+          return <Form className='form-control'>
 
-					<FormikControl 
-						control ='input'
-						label='Diameter:'
-						name='diameter'
-						type='number'
-						placeholder='30,5 cm'
-						min="30.5"
-						max="90.5"
-						step="10 cm"
-					/>
+            <FormikControl
+              control='input'
+              type='text'
+              label='Dish name'
+              name='dish'
+            />
 
-					<FormikControl
-						className='form-nubmer'
-						control ='input'
-						label='Spiciness scale:'
-						name='spicinessScale'
-						type='range'
-						min='1'
-						max='10'
-					/>
+            <FormikControl
+              control='date'
+              label='Choose a cooking time'
+              name='timeDish'
+            />
 
-					<FormikControl
-						className='form-nubmer'
-						control ='input'
-						label='Slices of bread:'
-						name='sliceBread'
-						type='number'
-						placeholder='0'
-						min='0'
-						max='10'
-					/>
+            <FormikControl
+              className='form-dish'
+              control='select'
+              label='Type of dish'
+              name='typeOfDish'
+              options={dropdownOptions}
+            />
 
+            {formik.values.typeOfDish === ID_PIZZA && (<>
+              <FormikControl
+                className='form-nubmer'
+                control='input'
+                label='Number of slices:'
+                name='numberOfSlices'
+                type='number'
+                placeholder='0'
+                min='0'
+                max="15"
+              />
+              <FormikControl
+                control='input'
+                label='Diameter:'
+                name='diameter'
+                type='number'
+                placeholder='30,5 cm'
+                min="30.5"
+                max="90.5"
+                step="10 cm"
+              />
+            </>)}
 
-						<button type='submit' disabled={!formik.isValid}>Submit</button>
-					</Form>
-				}
-			}
-		</Formik>
-	)
+            {formik.values.typeOfDish === ID_SOUP && (<>
+              <FormikControl
+                className='form-nubmer'
+                control='input'
+                label='Spiciness scale:'
+                name='spicinessScale'
+                type='range'
+                min='1'
+                max='10'
+              />
+            </>)}
+
+            {formik.values.typeOfDish === ID_SANDWICH && (<>
+              <FormikControl
+                className='form-nubmer'
+                control='input'
+                label='Slices of bread:'
+                name='sliceBread'
+                type='number'
+                placeholder='0'
+                min='0'
+                max='10'
+              />
+            </>)}
+
+            <button type='submit' disabled={!formik.isValid}>Submit</button>
+          </Form>
+        }
+      }
+    </Formik>
+  )
 }
 
 export default DishForm
